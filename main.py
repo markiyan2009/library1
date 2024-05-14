a5ea594f2c48caf7fde87c694735b90449d795e5
markiyan2009 <93789450+markiyan2009@users.noreply.github.com>
2024-05-14 22:54:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-
 import secrets
 
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException, status, Query
+from fastapi import FastAPI, Depends, HTTPException, status, Query, status
 
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Annotated
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 from database.ORM import ORM
-from database.models import Book
+from database.models import Book, User
 from schemas.book import BookBase, BookFull
+from schemas.user import UserBase, UserFull
+
 app = FastAPI(title="invent_products")
 library = {}
-users = [{"username":"ya","password":"1234"}]
+security = HTTPBasic()
 
 
-def check_credentials(username: str, password: str):
-    for user in users:
-        if username != user["username"] and password != user["password"]:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Помилка!!!!!")
-    return username
+def check_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credits.username, ORM.auth_username(credits.username))
+    correct_password = secrets.compare_digest(credits.password, ORM.auth_password(credits.password))
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credits.username
 
+@app.post("/auth")
+def auth(username=Depends(check_credentials)):
+    return {"msg":"Авторизація пройшла успішно"}
 
 @app.post("/add_book")
 def add_book(book : BookBase, username = Depends(check_credentials)):
@@ -40,8 +48,3 @@ def delete_book(name:str):
     
     return ORM.delete_author(name)
 
-@app.post("/add_user")
-def add_user(username:str, password:str):
-    
-    users.append({"username":username,"password":password})
-    return "Користувач доданий"
