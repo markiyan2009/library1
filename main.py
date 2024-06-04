01e0936737773828a3a6945a27088adcf63cb322
markiyan2009 <93789450+markiyan2009@users.noreply.github.com>
2024-06-04 23:48:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import secrets
 
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException, status, Query, status
-
+from fastapi import FastAPI, Depends, HTTPException, status, Query, status, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Annotated
+from fastapi.staticfiles import StaticFiles
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 from database.ORM import ORM
@@ -15,36 +16,67 @@ from schemas.user import UserBase, UserFull
 app = FastAPI(title="invent_products")
 library = {}
 security = HTTPBasic()
-
+templates = Jinja2Templates(directory="templates")
+app.mount("/static",StaticFiles(directory="templates/css"),"static")
 
 def check_credentials(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credits.username, ORM.auth_username(credits.username))
-    correct_password = secrets.compare_digest(credits.password, ORM.auth_password(credits.password))
-    if not (correct_username and correct_password):
+    user = ORM.auth(credentials.username, credentials.password)
+    print(user)
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credits.username
+    return credentials.username
 
-@app.post("/auth")
-def auth(username=Depends(check_credentials)):
-    return {"msg":"Авторизація пройшла успішно"}
 
-@app.post("/add_book")
-def add_book(book : BookBase, username = Depends(check_credentials)):
-    print(username)
-    book_db = Book(author = book.author,name = book.name, pages = book.pages)
-    ORM.add_book(book_db)
-    return "Книжка додана"
-@app.get("/{author}_books")
-def get_books_by_author(author : str):
-    return ORM.get_book_by_author(author)
-@app.patch("/refill_{name}")
-def refill(name : str, book : BookBase):
-    return ORM.refil_book(name=name, new_book=book)
-def delete_book(name:str):
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/submitform")
+async def index(login: str = Form(...), password: str = Form(...), credentials = Depends(check_credentials)):
     
-    return ORM.delete_author(name)
+    return RedirectResponse("/home", status_code=303)
 
+@app.get("/add_book")
+async def add_book(request : Request):
+    return templates.TemplateResponse("add_book.html",{"request":request})
+@app.post("/submit_add_book")
+async def add_book(author:str = Form(),name:str = Form(),pages:str = Form(), credentials = Depends(check_credentials)):
+    book_db = Book(author = author,name = name, pages = pages)
+    ORM.add_book(book_db)
+    return RedirectResponse("/home", status_code=303)
+
+
+@app.get("/home", response_class=HTMLResponse) 
+def home(request:Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+# @app.get("/author_books")
+# def get_books_by_author(request:Request):
+#     return templates.TemplateResponse("")
+@app.get("/refill_book")
+def refill(request : Request):
+    return templates.TemplateResponse("refill.html",{"request":request})
+@app.post("/submit_refill_book")
+def submit_refill(request:Request,old_book:str = Form(),author:str = Form(),name:str = Form(),pages:str = Form(),credentials = Depends(check_credentials)):
+    
+    ORM.refil_book(old_book,name,author,pages)
+    
+    return RedirectResponse("/home",status_code=303)
+    
+
+@app.get("/info")
+def info(request : Request):
+    return templates.TemplateResponse("info.html",{"request": request,"books" : ORM.get_all_books()})
+@app.get("/delete")
+def delete(request:Request):
+    return templates.TemplateResponse("delete.html",{"request":request})
+@app.post("/submit_delete")
+def submit_delete(name:str = Form(),credentials = Depends(check_credentials)):
+    ORM.delete_author(name)
+    return RedirectResponse("/home",status_code=303)
